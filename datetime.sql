b73b1e5625f45a217a4e59871ac55a2f893f0b43
as683513 <106982694+as683513@users.noreply.github.com>
2024-11-19 14:49:01 -0500

    Update datetime.sql

--- datetime.sql
+++ datetime.sql
@@ -22,3 +22,15 @@ SELECT
     RIGHT('0' + CAST((DATEDIFF(SECOND, StartDateTimeOffset, EndDateTimeOffset) % 3600) / 60 AS VARCHAR), 2) + ':' +
     RIGHT('0' + CAST((DATEDIFF(SECOND, StartDateTimeOffset, EndDateTimeOffset) % 60) AS VARCHAR), 2) AS DurationFormatted
 FROM YourTable;
+
+
+
+
+
+--Method 2: Truncate Seconds to the Start of the Minute
+--If you only need to truncate (not round) the time to the beginning of the minute:
+
+SELECT 
+    YourDateTimeField,
+    DATEADD(MINUTE, DATEDIFF(MINUTE, 0, YourDateTimeField), 0) AS TruncatedToMinute
+FROM YourTable;
